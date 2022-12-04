0e45701b56b4e2e55674af417bb0e88f4c0f9860
Cedric Blanchard <blanchard.ced@gmail.com>
2022-12-04 19:45:05 +0100

    update and finish - add reponsiveness

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const addBtn = document.querySelector('#add-btn');
-const cartBtn = document.querySelector('#cart-btn');
-
-addBtn.addEventListener('click', () => {
-  cartBtn.classList.add('new-item');
-});
-
+/* thumbnails interaction */
 const thumbnails = document.querySelectorAll('.thumbnail');
 const mainImg = document.querySelector('.main-img');
 
@@ -15,3 +9,28 @@ thumbnails.forEach(thumbnail => {
     thumbnail.classList.add('active');
   });
 });
+
+/* Cart interaction */
+const addBtn = document.querySelector('#add-btn');
+const cartBtn = document.querySelector('#cart-btn');
+
+addBtn.addEventListener('click', () => {
+  cartBtn.classList.add('new-item');
+});
+
+/* color interaction */
+const colors = document.querySelectorAll('.color');
+
+colors.forEach(color => {
+  color.addEventListener('click', e => {
+    colors.forEach(color => color.classList.remove('selected-color'));
+    e.target.classList.add('selected-color');
+  });
+});
+
+/* Mobile menu overflow */
+const mobileCheck = document.querySelector('#mobile-menu');
+
+mobileCheck.addEventListener('change', () =>
+  document.body.classList.toggle('overflow-hidden')
+);
